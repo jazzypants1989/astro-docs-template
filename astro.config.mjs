ea366500205e01bfd08c172787a4c956901e0906
Jesse Pence <jessepence@gmail.com>
2023-02-03 00:53:48 -0800

    one more shot

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from "@astrojs/tailwind"
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://inquisitive-centaur-fdce3f.netlify.app",
   integrations: [mdx(), tailwind()],
   server: {
     port: 3000,
