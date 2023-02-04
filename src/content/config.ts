8dd961dda8757949a99e79e536e018e7bd183224
Jesse Pence <jessepence@gmail.com>
2023-02-04 02:17:52 -0800

    yay, i fixed it

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,25 +1,24 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content"
 
 const docs = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		image: z.string().optional(),
-		video: z.string().optional(),
-		mdx: z.boolean().optional(),
-	}),
-});
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    // Transform string to Date object
+    pubDate: z
+      .string()
+      .or(z.date())
+      .transform((val) => new Date(val)),
+    updatedDate: z
+      .string()
+      .optional()
+      .transform((str) => (str ? new Date(str) : undefined)),
+    image: z.string().optional(),
+    video: z.string().optional(),
+  }),
+})
 
 export const collections = {
-	docs,
-};
+  docs,
+}
